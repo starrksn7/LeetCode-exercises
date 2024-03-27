81a859b354d5509de4dffc3a2b9b891b6209d219
Robert Weeks <92542753+starrksn7@users.noreply.github.com>
2024-03-26 20:34:38 -0400

    Time: 1 ms (97.94%), Space: 55.1 MB (51.58%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,21 +1,32 @@
 class Solution {
-    public int firstMissingPositive(int[] nums){
-        Set<Integer> set = new HashSet<>();
-        for(int num : nums){
-            if(num > 0){
-                set.add(num);
+    public int firstMissingPositive(int[] nums) {
+        boolean one = false;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 1){
+                one = true;
+            }
+
+            if(nums[i] < 1 || nums[i] > nums.length){
+                nums[i] = 1;
             }
         }
-        
-        int numToCheck = 1;
-        
-        while(true){
-            if(!set.contains(numToCheck)){
-                return numToCheck;
-            }
-            numToCheck++;
+
+        if(one == false){
+            return 1;
         }
+
+        for(int i = 0; i < nums.length; i++){
+            int x = Math.abs(nums[i]);
+            nums[x - 1] = -Math.abs(nums[x - 1]);
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0){
+                return i + 1;
+            }
+        }
+
+        return nums.length + 1;
     }
 }
-
-
